605b3002f2392b249df0d5b5513cb00eab65d7bb
tmquan5823 <tmquan0948628477@gmail.com>
2024-10-14 13:27:41 +0700

    Word service

--- src/controllers/wordController.js
+++ src/controllers/wordController.js
@@ -3,10 +3,12 @@ import NotFoundError from "../errors/NotFoundError.js";
 import Word from "../models/Word.js";
 import Kanji from "../models/Kanji.js";
 import { StatusCodes } from "http-status-codes";
+import { wordService } from "../services/wordService.js";
 
 export const getAllWords = async (req, res) => {
     try {
-        const words = await Word.find();
+        const words = wordService.getAllWords();
+
         res.status(StatusCodes.OK).send({
             status: "success",
             data: words
@@ -18,39 +20,13 @@ export const getAllWords = async (req, res) => {
 
 //GET /api/v1/words/{id}
 export const getWordById = async (req, res, next)=>{
-    const {id} = req.params;
     try{
-        if (!id || id.trim() === '') {
-            throw new BadRequestError("Bad Request!");
-        }
-
-        const result = await Word.findById(id);
-
-        if(result){
-            const kanjiInfoPromises = result.kanji.map(async (kanjiChar) => {
-                // Tìm tất cả các kanji phù hợp với text của từng kanji
-                const kanjiData = await Kanji.find({ text: kanjiChar });
- 
-                if (kanjiData && kanjiData.length > 0) {
-                    return kanjiData;
-                } else {
-                    return kanjiChar; 
-                }
-            });
-
-            const kanjiInfoArray = await Promise.all(kanjiInfoPromises);
-
-            const flattenedKanjiInfoArray = kanjiInfoArray.flat();
-
-            result.kanji = flattenedKanjiInfoArray || {};
-
-            return res.status(StatusCodes.OK).json({
-                status: "success",
-                data: result
-            });
-        } else {
-            throw new NotFoundError("Không tìm thấy dữ liệu!");
-        }
+        const result = await wordService.getWordById(req);
+        return res.status(StatusCodes.OK).json({
+            status: "success",
+            data: result
+        });
+       
     } catch(error){
         next(error);
     }
@@ -58,66 +34,12 @@ export const getWordById = async (req, res, next)=>{
 
 //GET /api/v1/words/search?text={}
 export const searchWord = async (req, res, next) => {
-    const { text } = req.query;
-
     try {
-        // Kiểm tra nếu không có từ khóa tìm kiếm
-        if (!text || text.trim() === "") {
-            throw new BadRequestError("Bad request");
-        }
-
-        // Xây dựng điều kiện truy vấn tìm kiếm theo tiền tố
-        const prefixCondition = {
-            $or: [
-                { text: { $regex: `^${text}`, $options: 'i' } },          // Tìm tiền tố trong trường 'text'
-                { hiragana: { $regex: `^${text}`, $options: 'i' } },      // Tìm tiền tố trong trường 'hiragana'
-                { romanji: { $regex: `^${text}`, $options: 'i' } },        // Tìm tiền tố trong 'romanji'
-                { "meaning.content": { $regex: `^${text}`, $options: 'i' } }  // Tìm tiền tố trong 'meaning.content'
-            ]
-        };
-
-        // Thực hiện truy vấn với điều kiện tiền tố
-        const prefixResults = await Word.find(prefixCondition).limit(10); // Giới hạn kết quả tối đa là 10
-
-        // Nếu đã có đủ 10 kết quả, trả về ngay
-        if (prefixResults.length >= 10) {
-            return res.status(StatusCodes.OK).json({
-                status: "success",
-                data: prefixResults});
-        }
-
-        // Nếu chưa đủ 10 kết quả, thực hiện truy vấn bổ sung theo hậu tố
-        const suffixCondition = {
-            $or: [
-                { text: { $regex: `${text}`, $options: 'i' } },          // Tìm hậu tố trong trường 'text'
-                { hiragana: { $regex: `${text}`, $options: 'i' } },      // Tìm hậu tố trong trường 'hiragana'
-                { romanji: { $regex: `${text}`, $options: 'i' } },        // Tìm hậu tố trong 'romanji'
-                { "meaning.content": { $regex: `${text}`, $options: 'i' } }  // Tìm hậu tố trong 'meaning.content'
-            ]
-        };
-
-        // Thực hiện truy vấn với điều kiện hậu tố
-        const suffixResults = await Word.find(suffixCondition).limit(10 - prefixResults.length); // Giới hạn theo số lượng còn lại
-
-        // Kết hợp kết quả từ cả hai truy vấn
-        const results = [...prefixResults, ...suffixResults];
-
-        const formattedResults = results.map(word => ({
-            _id: word._id,
-            text: word.text,
-            hiragana: word.hiragana[0],
-            meaning: word.meaning[0].content
-        }));
-        // Kiểm tra và trả về kết quả
-        if (results.length > 0) {
-            return res.status(StatusCodes.OK).json({
-                status: "success",
-                data: formattedResults
-            });
-        } else {
-            throw new NotFoundError("Không tìm thấy từ nào phù hợp.");
-        }
-
+        const result = await wordService.searchWord(req);
+        return res.status(StatusCodes.OK).json({
+            status: "success",
+            data: result
+        });
     } catch (error) {
         next(error);  
     }
---
+++ src/services/wordService.js
@@ -0,0 +1,107 @@
+import Word from "../models/Word.js";
+import Kanji from "../models/Kanji.js";
+import mongoose from "mongoose";
+import BadRequestError from "../errors/BadRequestError.js";
+import NotFoundError from "../errors/NotFoundError.js";
+
+const getAllWords = async () => {
+    try {
+        const words = await Word.find();
+        return words;
+    } catch(error) {
+        throw error;
+    };
+};
+
+const getWordById = async (data) => {
+    try {
+        const {id} = data.params;
+
+        if (!id || id.trim() === '' || !mongoose.isValidObjectId(id)) {
+            throw new BadRequestError("ID không hợp lệ!");
+        }
+        
+        const result = await Word.findById(id);
+
+        if(result){
+            const kanjiInfoPromises = result.kanji.map(async (kanjiChar) => {
+                // Tìm tất cả các kanji phù hợp với text của từng kanji
+                const kanjiData = await Kanji.find({ text: kanjiChar });
+ 
+                if (kanjiData && kanjiData.length > 0) {
+                    return kanjiData;
+                } else {
+                    return kanjiChar; 
+                }
+            });
+
+            const kanjiInfoArray = await Promise.all(kanjiInfoPromises);
+
+            const flattenedKanjiInfoArray = kanjiInfoArray.flat();
+
+            result.kanji = flattenedKanjiInfoArray || {};
+            return result
+        } else {
+            throw new NotFoundError("Không tìm thấy dữ liệu!");
+        }   
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+};
+
+const searchWord = async (data) => {
+    const { text } = data.query;
+    try {
+        if (!text || text.trim() === "") {
+            throw new BadRequestError("Bad request");
+        }
+
+        const prefixCondition = {
+            $or: [
+                { text: { $regex: `^${text}`, $options: 'i' } },          
+                { hiragana: { $regex: `^${text}`, $options: 'i' } },      
+                { romanji: { $regex: `^${text}`, $options: 'i' } },       
+                { "meaning.content": { $regex: `^${text}`, $options: 'i' } } 
+            ]
+        };
+
+        let results = await Word.find(prefixCondition).limit(10); 
+
+        if (results.length < 10) {
+            const suffixCondition = {
+                $or: [
+                    { text: { $regex: `${text}`, $options: 'i' } },          // Tìm hậu tố trong trường 'text'
+                    { hiragana: { $regex: `${text}`, $options: 'i' } },      // Tìm hậu tố trong trường 'hiragana'
+                    { romanji: { $regex: `${text}`, $options: 'i' } },        // Tìm hậu tố trong 'romanji'
+                    { "meaning.content": { $regex: `${text}`, $options: 'i' } }  // Tìm hậu tố trong 'meaning.content'
+                ]
+            };
+
+            const suffixResults = await Word.find(suffixCondition).limit(10 - results.length); // Giới hạn theo số lượng còn lại
+            
+            results = [...results, ...suffixResults];
+        }
+
+        const formattedResults = results.map(word => ({
+            _id: word._id,
+            text: word.text,
+            hiragana: word.hiragana[0],
+            meaning: word.meaning[0].content
+        }));
+
+        if (formattedResults.length > 0) {
+            return formattedResults;
+        } else {
+            throw new NotFoundError("Không tìm thấy từ nào phù hợp.");
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const wordService = {
+    getAllWords,
+    getWordById,
+    searchWord
+} 
